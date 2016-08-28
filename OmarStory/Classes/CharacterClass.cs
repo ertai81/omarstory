985e3e0698c7c282a6aa0c86c4b75ad2db8c5e91
ertai81 <jigosi3@gmail.com>
2016-08-28 13:01:03 +0200

    First upload

---
+++ OmarStory/Classes/CharacterClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace OmarStory.Classes
+{
+    public class CharacterClass
+    {
+        private string name;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+
+        private Image picture;
+        public Image Picture
+        {
+            get
+            {
+                return picture;
+            }
+        }
+    }
+}
---
+++ OmarStory/ViewModels/FooterGeneralViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OmarStory.ViewModels
+{
+    public class FooterGeneralViewModel
+    {
+       
+    }
+}
